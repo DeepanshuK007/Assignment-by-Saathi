f0b49f4e3652c623dbc9146c38e3352ed6c987f8
DEEPANSHU KUMAR <97106571+DeepanshuK007@users.noreply.github.com>
2024-04-18 00:56:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,13 +3,13 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-	apiKey: 'AIzaSyBFAU8t598MaIWG4P50liYTJ8IJSeM2nrM',
-	authDomain: 'saathi-app-008.firebaseapp.com',
-	projectId: 'saathi-app-008',
-	storageBucket: 'saathi-app-008.appspot.com',
-	messagingSenderId: '94039824377',
-	appId: '1:94039824377:web:a46ab2656e05e3b7b08f28',
-	measurementId: 'G-N5RNTWGYV1'
+	apiKey: 'AIzaSyBFA*********P50liYTJ8IJSeM2nrM',
+	authDomain: 'saathi-**********ebaseapp.com',
+	projectId: '*******',
+	storageBucket: 'saathi-*******8.appspot.com',
+	messagingSenderId: '9403***4377',
+	appId: '1:94039824377:web:a46ab26*******8f28',
+	measurementId: '*******WGYV1'
 };
 
 if (!firebase.apps.length) {
